ced44bdea9ff06516f84a3d1ebf751f41d5c0d6e
lokesh.k <94343329+lokeshwaran26@users.noreply.github.com>
2023-06-26 22:53:46 +0530

    Updated Home.jsx

--- frondend/src/pages/Home.jsx
+++ frondend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Home.css";
+import { toast } from "react-toastify";
 const Home = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -12,6 +13,18 @@ const Home = () => {
     const response = await axios.get("http://localhost:5000/users");
     if (response.status === 200) {
       setData(response.data);
+    }
+  };
+
+  const deleteUser = async (id) => {
+    if (window.confirm("Are you sure you want to delete the user!")) {
+      const response = await axios.delete(`http://localhost:5000/user/${id}`);
+      if (response.status === 200) {
+        toast.success(response.data);
+        getUsers();
+      } else {
+        toast.error("Something went wrong!");
+      }
     }
   };
   console.log(data);
@@ -37,11 +50,16 @@ const Home = () => {
                   <td>{item.email}</td>
                   <td>{item.contact}</td>
                   <td>
-                    <Link to={`/update/:${item.id}`}>
+                    <Link to={`/update/${item.id}`}>
                       <button className="btn btn-edit">Edit</button>
                     </Link>
-                    <button className="btn btn-delete">Edit</button>
-                    <Link to={`/view/:${item.id}`}>
+                    <button
+                      className="btn btn-delete"
+                      onClick={() => deleteUser(item.id)}
+                    >
+                      delete
+                    </button>
+                    <Link to={`/view/${item.id}`}>
                       <button className="btn btn-view">view</button>
                     </Link>
                   </td>
